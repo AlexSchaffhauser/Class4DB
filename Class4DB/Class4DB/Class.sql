14f1223350ef6e85f3f66ac0b4ba868698bf8562
AlexSchaffhauser <alexschaffhauser@gmail.com>
2019-11-27 18:37:31 -0500

    Added tables "Student", "Class", "Student_Class"

---
+++ Class4DB/Class4DB/Class.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Class]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ClassName] NCHAR(10) NOT NULL, 
+    [ClassNumber] NCHAR(10) NOT NULL
+)
---
+++ Class4DB/Class4DB/Student_Class.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Student_Class]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StudentId] INT NOT NULL, 
+    [ClassId] INT NOT NULL, 
+    CONSTRAINT [FK_Student_Class_ToStudent] FOREIGN KEY ([StudentId]) REFERENCES [Student]([Id]), 
+    CONSTRAINT [FK_Student_Class_ToClass] FOREIGN KEY ([ClassId]) REFERENCES [Class]([Id])
+)
