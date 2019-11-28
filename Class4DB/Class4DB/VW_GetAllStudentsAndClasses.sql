a7d91e0f5475f643105e9b8a047fe60ddf601e95
AlexSchaffhauser <alexschaffhauser@gmail.com>
2019-11-27 20:12:21 -0500

    Redesigned View

--- Class4DB/Class4DB/VW_GetAllStudentsAndClasses.sql
+++ Class4DB/Class4DB/VW_GetAllStudentsAndClasses.sql
@@ -1,2 +1,2 @@
 ﻿CREATE VIEW [dbo].[VW_GetAllStudentsAndClasses]
-	AS SELECT S.FirstName, S.LastName, C.ClassName FROM [Student] S JOIN [Student_Class] SC ON S.Id = SC.StudentId JOIN [Class] C ON SC.ClassId = C.Id
+	AS SELECT * FROM Student
